83b33c74e6a24a2fc6c5ce9a1072db589e5efe0b
Caio <67526531+CaioGBDesign@users.noreply.github.com>
2023-04-03 23:46:06 -0300

    Create da-en-quiz.js

---
+++ da-en-quiz.js
@@ -0,0 +1,77 @@
+let quiz_rules = [
+    "You only have 20 seconds to answer each question.",
+    "Once you mark each answer, you cannot unmark it.",
+    "You will receive 1 point for each correct answer."
+];
+
+let quiz_questions = [
+    // question 1
+    {
+        // question
+        question: "1. What is the main function of art direction in a design project?",
+
+        // options
+        options : [
+            "Creating a shared vision among the creative team.",
+            "Selecting the typography and color palette for the project.",
+            "Managing project delivery deadlines.",
+        ],
+
+        // correct answer
+        answer: 1,
+    },
+    
+    {
+        // write your question here
+        question: "2. What are the key skills required to become an art director?",
+    
+        options: [
+            "Knowledge of graphic design software.",
+            "Leadership and team management abilities.",
+            "Excellent writing skills.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write your question here
+        question: "3. What is the difference between art direction and graphic design?",
+    
+        options: [
+            "Art direction is responsible for managing the creative team, while graphic design is responsible for creating the visual elements.",
+            "Art direction is responsible for defining the aesthetic vision of the project, while graphic design is responsible for executing that vision.",
+            "Art direction focuses on creating concepts, while graphic design focuses on creating layouts.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write your question here
+        question: "4. What is the role of an art director in producing a TV commercial?",
+    
+        options: [
+            "Choosing the cast for the commercial.",
+            "Defining the plot of the commercial.",
+            "Developing the storyboard for the commercial.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write your question here
+        question: "5. What is the importance of art direction in creating a brand's visual identity?",
+    
+        options: [
+            "Art direction is responsible for defining the brand's logo.",
+            "Art direction helps ensure that all visual elements of the brand are consistent and cohesive.",
+            "Art direction is responsible for defining the brand's marketing strategy.",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    }
+    
+];
